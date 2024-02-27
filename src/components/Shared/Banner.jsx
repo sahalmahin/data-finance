43ca1f641f96fedaef7252bef839c1fddd79fe2f
sahalmahin <samirsatter0142@gmail.com>
2024-02-27 11:20:19 +0600

    banner

---
+++ src/components/Shared/Banner.jsx
@@ -0,0 +1,25 @@
+import { ReactTyped } from "react-typed";
+
+const Banner = () => {
+    return (
+        <div className="text-white">
+            <div className="max-w-[800px] w-full h-screen mt-[-96px] mx-auto text-center flex flex-col justify-center">
+                <p className="text-[#00df9a] font-bold p-2">Growing With Data Analytics</p>
+                <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">Grow with data</h1>
+                <div className="flex justify-center items-center gap-3">
+                    <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">Fast, Flexible Financing For</p>
+                    <ReactTyped
+                        className="md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2"
+                        strings={['BTB', 'BTC', 'SASS']}
+                        typedSpeed={500}
+                        backSpeed={500}
+                        loop />
+                </div>
+                <p className="md:text-2xl text-xl font-bold text-gray-500">Monitor your data analytics to increase revenue for BTB, BTC & SASS platforms.</p>
+                <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">Get Started</button>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;

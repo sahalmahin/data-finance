ba99bf4ac2bb8af3a62b10ac83d5f5081864f80d
sahalmahin <samirsatter0142@gmail.com>
2024-02-27 11:19:41 +0600

    navbar

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Analytics from "../Shared/Analytics";
+import Banner from "../Shared/Banner";
+
+const Home = () => {
+    return (
+        <div>
+            <Banner/>
+            <Analytics/>
+        </div>
+    );
+};
+
+export default Home;

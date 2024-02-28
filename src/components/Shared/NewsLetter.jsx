326f3ffd7260f6d3c9f393f72bcd1af5f2ff5dd6
sahalmahin <samirsatter0142@gmail.com>
2024-02-28 07:14:19 +0600

    news letter header

--- src/components/Shared/NewsLetter.jsx
+++ src/components/Shared/NewsLetter.jsx
@@ -1,7 +1,12 @@
 const NewsLetter = () => {
     return (
-        <div>
-            
+        <div className="w-full py-16 text-white">
+            <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3">
+                <div>
+                    <h1 className="md:text-5xl sm:text-4xl text-xl">Want tips & tricks to optimize your flow?</h1>
+                    <p>Sign up to your newsletter & stay up to date.</p>
+                </div>
+            </div>
         </div>
     );
 };
